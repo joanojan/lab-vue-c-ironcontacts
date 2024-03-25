de0590389c6de9128cd769133c5d1cba9227ef1f
Joan Vila Valls <joan.o.jan@gmail.com>
2024-03-25 19:00:21 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,63 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import contactsJson from '@/contacts.json'
+import ContactsTable from '@/components/ContactsTable.vue'
+import { ref, provide } from 'vue'
+
+const contacts = ref([])
+
+contacts.value = contactsJson.filter((contact, index) => index < 5)
+
+provide('contacts', contacts)
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<template>
+  <div class="container">
+    <h1>IronContacts</h1>
+    <table>
+      <th class="picture"><h2>Picture</h2></th>
+      <th class="name">
+        <h2>Name</h2>
+      </th>
+      <th class="popularity">
+        <h2>Popularity</h2>
+      </th>
+      <tr v-for="contact in contacts" :key="contact">
+        <td><img :src="contact.pictureUrl" alt="artist picture"></td>
+        <td>
+          <h3>{{ contact.name }}</h3>
+        </td>
+        <td>
+          <h3>{{ contact.popularity.toFixed(2) }}</h3>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+h1 {
+  font-size: 80px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+h2 {
+  font-weight: bold;
+  font-size: 30px;
+}
+h3 {
+  font-weight: 400;
+  font-size: 30px;
+}
+
+td {
+  text-align: center;
+  width: 150px;
+}
+
+img {
+  width: 110px;
 }
 </style>
